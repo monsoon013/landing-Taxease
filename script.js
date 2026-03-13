7235d32200c160858bad94cfe160eb51d34a5806
monsoon013 <cael.corredera.perez@gmail.com>
2026-03-13 19:17:47 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,1 @@
-const  menuIcon = document.querySelector('.menu-toggle');
-const navLinks = document.querySelector('.navegacion');
 
-menuIcon.addEventListener('click', () => {
-    navLinks.classList.toggle('show');
-})
